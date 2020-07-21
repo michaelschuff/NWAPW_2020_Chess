4775edcf2b145bdddb6a3f8a2ae3c0af720627c3
Michael Schuff <mschuff0881@gmail.com>
2020-07-21 13:21:03 -0700

    no message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-const { resolve6 } = require('dns');
+var ip = require("ip");
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
@@ -27,6 +27,7 @@ http.createServer(function (req, res) {
       console.log('Body: ' + body)
       res.writeHead(200, {'Content-Type': 'text/html'})
       res.end('post received')
+      console.log('post received')
     })
   }
 
