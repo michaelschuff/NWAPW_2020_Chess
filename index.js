8907814541eaa9e5e8dec435c9fa41b4fd74b73f
Michael Schuff <mschuff0881@gmail.com>
2020-07-22 14:47:04 -0700

    added comments and removed old code

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+//load/setup modules
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 
-
+//vars
 var board = [
   ['WR','WN','WB','WQ','WK','WB','WN','WR'],
   ['WP','WP','WP','WP','WP','WP','WP','WP'],
@@ -19,12 +20,14 @@ flippedBoard = [[]];
 wTurn = true;
 playing = false;
 var clients;
+
+//redirect player to index.html page
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/index.html');
 });
 
 io.on('connection', function(socket){
-  if (!playing) {
+  if (!playing) {//create a room with two clients
     socket.join('game1');
     clients = Object.keys(socket.adapter.rooms['game1'].sockets);
     console.log("user ", clients.length, " has connected");
@@ -32,7 +35,7 @@ io.on('connection', function(socket){
   if (clients.length == 2) {
     playing = true;
     wTurn = !wTurn;
-    flippedBoard = [];
+    flippedBoard = [];//create a board that is rotated 180 degrees, so black is on the bottom. will be sent to black player
     for (i = 7; i >= 0; i--) { 
       var arr = [];
       for (j = 7; j >= 0; j--) {
@@ -40,14 +43,17 @@ io.on('connection', function(socket){
       }
       flippedBoard.push(arr);
     }
+    //send boards to clients
     io.to(clients[0]).emit('board update', board);
     io.to(clients[1]).emit('board update', flippedBoard);
   }
   socket.on('move', function(move) {
     if (playing) {
-      alphabet = ['a','b','c','d','e','f','g','h']
+      alphabet = ['a','b','c','d','e','f','g','h']//convert game move (e2e4) into two arrays, each with an x,y coord
       from = [alphabet.indexOf(move[0]), parseInt(move[1])-1]
       to = [alphabet.indexOf(move[2]), parseInt(move[3])-1]
+
+      //swap the from square and the to square
       var temp = board[from[1]][from[0]]
       board[from[1]][from[0]] = board[to[1]][to[0]]
       board[to[1]][to[0]] = temp;
@@ -56,7 +62,7 @@ io.on('connection', function(socket){
 
 
     wTurn = !wTurn;
-    flippedBoard = [];
+    flippedBoard = [];//create a board that is rotated 180 degrees, so black is on the bottom. will be sent to black player
     for (i = 7; i >= 0; i--) { 
       var arr = [];
       for (j = 7; j >= 0; j--) {
@@ -64,9 +70,9 @@ io.on('connection', function(socket){
       }
       flippedBoard.push(arr);
     }
+    //send boards to clients
     io.to(clients[0]).emit('board update', board);
     io.to(clients[1]).emit('board update', flippedBoard);
-    // io.emit('chat message', msg);
   });
 });
 
