bc21f0593321a99e4229f129b596bdc44897cedb
Michael Schuff <mschuff0881@gmail.com>
2020-07-26 20:48:40 -0700

    changed strings to lowercase

--- Chess_Logic.js
+++ Chess_Logic.js
@@ -6,14 +6,14 @@
 
 function illegalMoveCheck (board, move){
 	function findColor(piece){
-		var white = ["WK", "WQ", "WB", "WN", "WR", "WP"];
-		var black = ["BK", "BQ", "BB", "BN", "BR", "BP"];
+		var white = ['wk', 'wq', 'wb', 'wn', 'wr', 'wp'];
+		var black = ['bk', 'bq', 'bb', 'bn', 'br', 'bp'];
 		for (var i = 0; i < 6; i++){
 			if (tempboard[squareLoc] == white[i]){
-				return "W";
+				return 'w';
 			}
 			if (tempboard[squareLoc] == black[i]){
-				return "B";
+				return 'b';
 			}
 		}
 	}
@@ -31,25 +31,25 @@ function illegalMoveCheck (board, move){
 		var oppo, kLoc, changeLoc = 0;
 
 		for (var y = 0; y < 64; y++){
-			if (board[y] == color + "K"){
+			if (board[y] == color + 'k'){
 				kLoc = y;
 			}
 		}
-		if (color == "W"){
-			oppo = "B";
-			if (board[kLoc + 7] == "BP"){
+		if (color == 'w'){
+			oppo = 'b';
+			if (board[kLoc + 7] == 'bp'){
 				return true;
 			}
-			else if (board[kLoc + 9] == "BP"){
+			else if (board[kLoc + 9] == 'bp'){
 				return true;
 			}
 		}
 		else {
-			oppo = "W";
-			if (board[kLoc - 7] == "WP"){
+			oppo = 'w';
+			if (board[kLoc - 7] == 'wp'){
 				return true;
 			}
-			else if (board[kLoc - 9] == "WP"){
+			else if (board[kLoc - 9] == 'wp'){
 				return true;
 			}
 		}
@@ -57,13 +57,13 @@ function illegalMoveCheck (board, move){
 			while ((kLoc + changeLoc + horizontal[h] >= 0) && (kLoc + changeLoc + horizontal[h] <= 7)){
 				changeLoc += horizontal[h];
 
-				if (board[kLoc + changeLoc] == oppo + "R"){
+				if (board[kLoc + changeLoc] == oppo + 'r'){
 					return true;
 				}
-				else if (board[kLoc + changeLoc] == oppo + "Q"){
+				else if (board[kLoc + changeLoc] == oppo + 'q'){
 					return true;
 				}
-				else if (board[kLoc + changeLoc] != "  "){
+				else if (board[kLoc + changeLoc] != '__'){
 					break;
 				}
 			}
@@ -72,13 +72,13 @@ function illegalMoveCheck (board, move){
 		for (var d = 0; d < 4; d++){
 			while ((kLoc + changeLoc + diagonal[d] >= 0) && (kLoc + changeLoc + diagonal[d] <= 7)){
 				changeLoc += diagonal[d];
-				if (board[kLoc + changeLoc] == oppo + "B"){
+				if (board[kLoc + changeLoc] == oppo + 'b'){
 					return true;
 				}
-				else if (board[kLoc + changeLoc] == oppo + "Q"){
+				else if (board[kLoc + changeLoc] == oppo + 'q'){
 					return true;
 				}
-				else if (board[kLoc + changeLoc] == "  "){
+				else if (board[kLoc + changeLoc] == '__'){
 					break;
 				}
 			}
@@ -88,19 +88,19 @@ function illegalMoveCheck (board, move){
 
 		for(var k = 0; k < 8; k++){
 			if ((kLoc + knight[k] >= 0) && (kLoc + knight[k] <=7)){
-				if (board[kLoc + knight[k]] == oppo + "N"){
+				if (board[kLoc + knight[k]] == oppo + 'n'){
 					return true;
 				}
 			}
 		}
 		for (var alld = 0; alld < 4; alld++){
 			if ((kloc + horizontal[alld] >= 0) && (kLoc + horizontal[alld] <= 7)){
-				if (board[kLoc + horizontal[alld]] == oppo + "K"){
+				if (board[kLoc + horizontal[alld]] == oppo + 'k'){
 					return true;
 				}
 			}
 			if ((kLoc + diagonal[alld] >= 0) && (kLoc + diagonal[alld] <= 7)){
-				if (board[kLoc + diagonal[alld]] == oppo + "K"){
+				if (board[kLoc + diagonal[alld]] == oppo + 'k'){
 					return true;
 				}
 			}
@@ -110,22 +110,22 @@ function illegalMoveCheck (board, move){
     }
 	function legalmovesincheck(legalmoves, board, pieceLoc, squareLoc){
 		
-		if ((tempboard[pieceLoc] == "WK" || tempboard[pieceLoc] == "BK") && (squareLoc % 8) == 6 && (pieceLoc % 8) == 4){
-			tempboard[(Math.floor(pieceLoc / 8) * 8) + 5] = "WR";
-			tempboard[(Math.floor(pieceLoc / 8) * 8) + 7] = "  ";
+		if ((tempboard[pieceLoc] == 'wk' || tempboard[pieceLoc] == 'bk') && (squareLoc % 8) == 6 && (pieceLoc % 8) == 4){
+			tempboard[(Math.floor(pieceLoc / 8) * 8) + 5] = 'wr';
+			tempboard[(Math.floor(pieceLoc / 8) * 8) + 7] = '__';
 		}
-		else if ((tempboard[pieceLoc] == "WK" || tempboard[pieceLoc] == "BK") && ((squareLoc % 8) == 2) && ((pieceLoc % 8) == 4)){
-			tempboard[(Math.floor(pieceLoc / 8) * 8) + 3]= "WR";
-			tempboard[(Math.floor(pieceLoc / 8) * 8) + 0]= "  ";
+		else if ((tempboard[pieceLoc] == 'wk' || tempboard[pieceLoc] == 'bk') && ((squareLoc % 8) == 2) && ((pieceLoc % 8) == 4)){
+			tempboard[(Math.floor(pieceLoc / 8) * 8) + 3]= 'wr';
+			tempboard[(Math.floor(pieceLoc / 8) * 8) + 0]= '__';
 		}
-		else if (tempboard[pieceLoc]== "WP" && tempboard[squareLoc] == "  " && tempboard[squareLoc + 8] == "BP"){
-			tempboard[squareLoc + 8] = "  ";
+		else if (tempboard[pieceLoc]== 'wp' && tempboard[squareLoc] == '__' && tempboard[squareLoc + 8] == 'bp'){
+			tempboard[squareLoc + 8] = '__';
 		}
-		else if (tempboard[pieceLoc]== "BP" && tempboard[squareLoc] == "  " && tempboard[squareLoc - 8] == "WP"){
-			tempboard[squareLoc - 8] = "  ";
+		else if (tempboard[pieceLoc]== 'bp' && tempboard[squareLoc] == '__' && tempboard[squareLoc - 8] == 'wp'){
+			tempboard[squareLoc - 8] = '__';
 		}
 		tempboard[squareLoc] = tempboard[pieceLoc];
-		tempboard[pieceLoc] = "  "; //move the piece on the temporary board, same code as the movepiece funciton
+		tempboard[pieceLoc] = '__'; //move the piece on the temporary board, same code as the movepiece funciton
 		
 		
 		if (!kingincheck(tempboard, findColor(tempboard[squareLoc]))){//check if the king is in check
@@ -140,27 +140,27 @@ function illegalMoveCheck (board, move){
 			tempboard[y]=board[y];
 		}
 		var ncolor=0;//this is a number color, which is 0 or 7, based on what color. Basically lets us not have to write the same code with the numbers changed
-		if(color == "W"){
+		if(color == 'w'){
 			ncolor = 7;
 		}
 		if(!kingincheck(board,color)){//if the king isnt in check
-			if(leftORright == "L"){//if he passed all the requirements for calstling to the left
+			if(leftORright == 'L'){//if he passed all the requirements for calstling to the left
 				tempboard[(ncolor * 8) + 3] = tempboard[(ncolor* 8) + 4];
-				tempboard[(ncolor * 8) + 4]= "  ";//move the king one to the left
+				tempboard[(ncolor * 8) + 4]= '__';//move the king one to the left
 				if(!kingincheck(tempboard, color)){//if hes not in check
 					tempboard[(ncolor * 8) + 2] = tempboard[(ncolor * 8) + 3];//move him again
-					tempboard[(ncolor * 8) + 3] = "  ";
+					tempboard[(ncolor * 8) + 3] = '__';
 					if(!kingincheck(tempboard, color)){//if hes not in check
 						legalmoves = addmove(legalmoves, (4 + 8 * ncolor), 2 + (8 * ncolor));//add castling to the left as a legal move
 					}
 				}
 			}
-			if(leftORright == "R"){//basically the same code as the above, but it check to the right
+			if(leftORright == 'r'){//basically the same code as the above, but it check to the right
 				tempboard[(ncolor * 8) + 5]=tempboard[(ncolor * 8) + 4];
-				tempboard[(ncolor * 8) + 4]="  ";
+				tempboard[(ncolor * 8) + 4]='__';
 				if(!kingincheck(tempboard, color)){
 					tempboard[(ncolor * 8) + 6] = tempboard[(ncolor * 8) + 5];
-					tempboard[(ncolor * 8) + 5] = "  ";
+					tempboard[(ncolor * 8) + 5] = '__';
 					if(!kingincheck(tempboard, color)){
 						legalmoves = addmove(legalmoves, 4 + (8 * ncolor), 6 + (8 * ncolor));
 					}
@@ -172,38 +172,38 @@ function illegalMoveCheck (board, move){
 
 	function pawnlegalmoves(board, legalmoves, pieceLoc){//gets the legal pawn moves if the piece you have selected is a pawn
 		var color = findColor(board[pieceLoc]);//get the color of the piece you have selected
-		if(color == "W"){//pawns move differently based on color so we have to have 2 seperate legal move finders
-			if(board[pieceLoc + 8] == "  "){// if the square in front of it is empty
+		if(color == 'w'){//pawns move differently based on color so we have to have 2 seperate legal move finders
+			if(board[pieceLoc + 8] == '__'){// if the square in front of it is empty
 				legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc + 8);//and when you move the piece it isnt in check, add the move
-				if(board[pieceLoc + 16] == "  " && Math.floor(pieceLoc / 8) == 6){//if the square 2 squares in front isnt occupied and the pawn is on its starting square, then it can move two squares
+				if(board[pieceLoc + 16] == '__' && Math.floor(pieceLoc / 8) == 6){//if the square 2 squares in front isnt occupied and the pawn is on its starting square, then it can move two squares
 					legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc + 16);
 				}
 			}
 			if(pieceLoc % 8 > 0){//if it isnt on the left edge, check if it can take a piece diagonally to the left
-				if(findColor(board[pieceLoc + 7]) == "B"){//if that square is black
+				if(findColor(board[pieceLoc + 7]) == 'b'){//if that square is black
 					legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc + 7);// if its not in check move the piece
 				}
 			}
 			if(pieceLoc % 8 < 7){//same code as above, but checks the right side
-				if(findColor(board[pieceLoc + 9]) == "B"){
+				if(findColor(board[pieceLoc + 9]) == 'b'){
 					legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc + 9);
 				}
 			}
 		}
 		else{//same code as above but for black pawns
-			if(board[pieceLoc - 8] == "  "){
+			if(board[pieceLoc - 8] == '__'){
 				legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc - 8);
-				if(board[pieceLoc - 16] == "  " && Math.floor(pieceLoc / 8) == 1){
+				if(board[pieceLoc - 16] == '__' && Math.floor(pieceLoc / 8) == 1){
 					legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc - 16);
 				}
 			}
 			if(pieceLoc % 8 > 0){
-				if(findColor(board[pieceLoc - 9]) == "W"){
+				if(findColor(board[pieceLoc - 9]) == 'w'){
 					legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc - 9);
 				}
 			}
 			if(pieceLoc % 8 < 7){
-				if(findColor(board[pieceLoc - 7]) == "W"){
+				if(findColor(board[pieceLoc - 7]) == 'w'){
 					legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc - 7);
 				}
 			}
@@ -232,7 +232,7 @@ function illegalMoveCheck (board, move){
 				if(findColor(board[pieceLoc]) != findColor(board[pieceLoc + changeLoc])){//if the square you are checking is empty or of opposite color
 					legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc + changeLoc);//add the moves
 				}
-				if(board[pieceLoc + changeLoc] != "  "){//if the square isnt empty, meaning the rook cannot move to any square beyond this one (because pieces cant jump over pieces), break out of the loop
+				if(board[pieceLoc + changeLoc] != '__'){//if the square isnt empty, meaning the rook cannot move to any square beyond this one (because pieces cant jump over pieces), break out of the loop
 					break;
 				}
 			}
@@ -250,7 +250,7 @@ function illegalMoveCheck (board, move){
 				if(findColor(board[pieceLoc]) != findColor(board[pieceLoc + changeLoc])){
 					legalmoves = legalmovesincheck(board, legalmoves, pieceLoc, pieceLoc + changeLoc);
 				}
-				if(board[pieceLoc + changeLoc] != "  "){
+				if(board[pieceLoc + changeLoc] != '__'){
 					break;
 				}
 			}
@@ -276,14 +276,14 @@ function illegalMoveCheck (board, move){
 				}
 			}
 		}
-		if(board[pieceLoc] == "WK"){//if the piece is white
+		if(board[pieceLoc] == 'wk'){//if the piece is white
 			for(var n = 0; n < 64; n++){//this basically says if you moved your rook or king before, you cant castle
-				if(board[n] == "WK" && n != 4){
+				if(board[n] == 'wk' && n != 4){
 					left = false;
 					right = false;
 					break;
 				}
-				if(board[n] == "WR"){
+				if(board[n] == 'wr'){
 					if(n != 0){
 						left = false;
 						break;
@@ -294,23 +294,23 @@ function illegalMoveCheck (board, move){
 					}
 				}
 			}
-			if(left && board[3] == "  " && board[2] == "  " && board[1] == "  "){ //this checks if the square between castling are empty
-				legalmoves = castling(board, legalmoves, "L", "W");
+			if(left && board[3] == '__' && board[2] == '__' && board[1] == '__'){ //this checks if the square between castling are empty
+				legalmoves = castling(board, legalmoves, 'l', 'w');
 			}
-			if(right && board[5] == "  " && board[6] == "  "){// same but for the other side
-				legalmoves = castling(board, legalmoves, "R", "W");
+			if(right && board[5] == '__' && board[6] == '__'){// same but for the other side
+				legalmoves = castling(board, legalmoves, 'r', 'w');
 			}
 		}
 		else{ //if the piece is black, same code but different values
 			for(var n = 0; n < 64; n++){
-				if(board[n] == "BK"){
+				if(board[n] == 'bk'){
 					if (n != 60){
 						left = false;
 						right = false;
 						break;
 					}
 				}
-				if(board[n] == "BR"){
+				if(board[n] == 'br'){
 					if  (n != 56){
 						left = false;
 						break;
@@ -322,11 +322,11 @@ function illegalMoveCheck (board, move){
 				}
 				
 			}
-			if(left && board[59] == "  " && board[58] == "  " && board[57] == "  "){
-				legalmoves = castling(board, legalmoves, "L", "B");
+			if(left && board[59] == '__' && board[58] == '__' && board[57] == '__'){
+				legalmoves = castling(board, legalmoves, 'l', 'B');
 			}
-			if(right && board[61] == "  " && board[62] == "  "){
-				legalmoves = castling(board, legalmoves, "R", "B");
+			if(right && board[61] == '__' && board[62] == '__'){
+				legalmoves = castling(board, legalmoves, 'r', 'b');
 			}
 		}
         return legalmoves;
@@ -335,22 +335,22 @@ function illegalMoveCheck (board, move){
 	function getlegalmoves(board, legalmoves, pieceLoc){// this takes in the piece you have selected
 		legalmoves.clear();
 		var piece = board[pieceLoc];//set the piece
-		if(piece == "WP" || piece == "BP"){//if it piece is == n, then send it to function that finds piece n's legal moves
+		if(piece == 'wp' || piece == 'bp'){//if it piece is == n, then send it to function that finds piece n's legal moves
 			legalmoves = pawnlegalmoves(board, legalmoves, pieceLoc);
 		}
-		else if(piece == "WR" || piece == "BR"){
+		else if(piece == 'wr' || piece == 'br'){
 			legalmoves = rooklegalmoves(board, legalmoves, pieceLoc);
 		}
-		else if(piece == "WN" || piece == "BN"){
+		else if(piece == 'wn' || piece == 'bn'){
 			legalmoves = knightlegalmoves(board, legalmoves, pieceLoc);
 		}
-		else if(piece == "WB" || piece == "BB"){
+		else if(piece == 'wb' || piece == 'bb'){
 			legalmoves = bishoplegalmoves(board, legalmoves, pieceLoc);
 		}
-		else if(piece == "WQ" || piece == "BQ"){
+		else if(piece == 'wq' || piece == 'bq'){
 			legalmoves = queenlegalmoves(board, legalmoves, pieceLoc);
 		}
-		else if(piece == "WK" || piece == "BK"){
+		else if(piece == 'wk' || piece == 'bk'){
 			legalmoves = kinglegalmoves(board, legalmoves, pieceLoc);
 		}
 		return legalmoves;
