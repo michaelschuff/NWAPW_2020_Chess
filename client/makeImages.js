307774f7544974532338e0ee2370ba88817b8f26
Michael Schuff <mschuff0881@gmail.com>
2020-07-28 12:53:56 -0700

    added example for dynamically creating an image

---
+++ client/makeImages.js
@@ -0,0 +1,6 @@
+function createBoard() {
+    var img = document.createElement('img');
+    img.id = 'board';
+    img.src = '/client/imgs/board.png';
+    document.body.appendChild(img);
+}
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 //load/setup modules
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 
-//redirect player to index.html page
+app.use(express.static(__dirname));//let client use files
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/client/index.html');
+  res.sendFile(__dirname + '/client/index.html');//redirect to index.html
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket){//when communication is established
   
 });
 
