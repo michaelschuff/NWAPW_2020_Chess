41de3f81196d1b4ba19c0c52a87d873a403c0d0c
Michael Schuff <mschuff0881@gmail.com>
2020-07-29 09:09:58 -0700

    no message

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
-const { v1: uuidv1 } = require('uuid');
 
 //vars
 
